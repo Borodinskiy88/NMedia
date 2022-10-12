605a60db9c804db6618c1c506a14474220a5c0e9
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-10-13 00:12:22 +0300

    Лайки и шары

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,7 +1,7 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,26 +11,39 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post (
+        val post = Post(
             id = 1,
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "6 октября в 16:36",
             likedByMe = false
-                )
+        )
 
         with(binding) {
             author.text = post.author
             published.text = post.published
             content.text = post.content
             if (post.likedByMe) {
-                liked.setImageResource(R.drawable.ic_liked_favorite_24)
+                liked?.setImageResource(R.drawable.ic_liked_favorite_24)
             }
-            liked.setOnClickListener {
-                post.likedByMe != post.likedByMe
+            likedCount?.text = post.likes.toString()
+
+
+            liked?.setOnClickListener {
+                post.likedByMe = !post.likedByMe
                 liked.setImageResource(
-                    if (post.likedByMe)R.drawable.ic_liked_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                    if (post.likedByMe) R.drawable.ic_liked_favorite_24 else R.drawable.ic_baseline_favorite_border_24
                 )
+                if (post.likedByMe) post.likes++ else post.likes--
+                likedCount?.text = post.likes.toString()
+            }
+
+
+            shareCount?.text = post.share.toString()
+
+            share?.setOnClickListener {
+                post.share++
+                shareCount?.text = post.share.toString()
             }
         }
     }
--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -5,5 +5,8 @@ data class Post (
     val author: String,
     val content: String,
     val published: String,
+    var likes:Int = 0,
+    var share:Int = 0,
     var likedByMe: Boolean = false
+
 )
