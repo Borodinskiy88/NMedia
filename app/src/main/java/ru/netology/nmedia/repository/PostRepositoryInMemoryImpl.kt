f3b1046396a85fe8b968a8377f9449194cb2be48
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-11-16 19:20:55 +0300

    storage

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -43,6 +43,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
     private val data = MutableLiveData(posts)
 
     override fun getAll(): LiveData<List<Post>> = data
+
     override fun likeById(id: Long) {
         posts = posts.map {
             if (it.id != id) it else it.copy(
